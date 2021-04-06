ebd61791010cdd5cab61273c68b47a88f809afe7
Nafi-Amaan-Hossain <68556185+Nafi-Amaan-Hossain@users.noreply.github.com>
2021-04-06 12:58:59 +0530

    Handled possible exception
    If user has a typo and no Docker installation is found, error can occur.

--- test_contained_user.sh
+++ test_contained_user.sh
@@ -4,7 +4,7 @@
 # [*] Docker Installation: https://medium.com/@airman604/installing-docker-in-kali-linux-2017-1-fbaa4d1447fe
 
 
-
+mainFunc(){
 # script starts here
 read -p "Do you already have docker installed? (yes, no)" answer
 if [ $answer == "no" ]; then
@@ -12,8 +12,12 @@ if [ $answer == "no" ]; then
   echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list ; \
   sudo apt-get update ; \
   sudo apt-get install docker-ce
-else
+elif [$answer == "yes" ]; then
   echo "You already have Docker installed. Moving on to the next step."
+else
+  echo "Please enter 'yes' or 'no'";\
+  mainFunc()
 fi
+}
 
-# to be continued... 5 years?
+mainFunc()
